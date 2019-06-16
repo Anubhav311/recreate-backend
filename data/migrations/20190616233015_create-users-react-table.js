2874b7f35dd9d9640ce5c52cade968632be81ab9
Anubhav311 <anubhavbhambri1@gmail.com>
2019-06-17 00:14:40 +0530

    added foreigh keys to users_react table

--- data/migrations/20190616233015_create-users-react-table.js
+++ data/migrations/20190616233015_create-users-react-table.js
@@ -2,8 +2,18 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('users_react', tbl => {
         tbl.increments();
-        tbl.integer('user_id').notNullable();
-        tbl.string('react_course_file_id', 5000);
+        tbl.integer('user_id')
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE');
+        tbl.integer('react_course_file_id')
+            .notNullable()
+            .references('id')
+            .inTable('react_course')
+            .onDelete('RESTRICT')
+            .onUpdate('CASCADE');
         tbl.string('submitted_code');
     })
 };
--- data/seeds/01-create_reactcourse.js
+++ data/seeds/01-create_reactcourse.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('react_course').insert([
-        {section: 'rowValue1', commentary: 'this is some randome commentary one', code: 'this is some random code one'},
-        {section: 'rowValue2', commentary: 'this is some randome commentary two', code: 'this is some random code two'},
-        {section: 'rowValue3', commentary: 'this is some randome commentary three', code: 'this is some random code three'}
+        {section: 1, commentary: 'this is some random commentary one', code: 'this is some random code one'},
+        {section: 1, commentary: 'this is some random commentary two', code: 'this is some random code two'},
+        {section: 1, commentary: 'this is some random commentary three', code: 'this is some random code three'}
       ]);
     });
 };
