9b6297d681231d1414d5fecb646c9a55f193cbed
Anubhav311 <anubhavbhambri1@gmail.com>
2019-06-17 04:44:44 +0530

    created POST route for users registration

--- endpoints/users-router.js
+++ endpoints/users-router.js
@@ -2,6 +2,18 @@ const router = require('express').Router();
 
 const Users = require('../api/users-api');
 
+//users can create account with this endpoint
+router.post('/', (req, res) => {
+    Users.insert(req.body)
+        .then(dbRes => {
+            res.status(400).json(dbRes)
+        })
+        .catch(err => {
+            res.status(500).json(err.message)
+        })
+})
+
+//for administrative use
 router.get('/', (req, res) => {
     Users.getMultiple()
         .then(dbRes => {
